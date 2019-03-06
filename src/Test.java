6b1eafa8c7881ee6b3c7055a33566257f4719a69
Denitsa Laleva <delaleva@gmail.com>
2019-03-06 15:57:49 +0000

    Tested methods

--- src/Test.java
+++ src/Test.java
@@ -63,35 +63,152 @@ public class Test {
         }
     }
 
+    public static Float calculateNumeratorForSimilarityFunction(Integer item1, Integer item2) {
+        HashMap<Integer, RatingTuple> userRatingsForTwoItems = getUserRatingsForTwoItems(item1, item2);
+        Float sum = 0.0f;
+
+        for (Integer user : userRatingsForTwoItems.keySet()) {
+            Float userAverageRating = averageRatings.get(user);
+            //System.out.println(userAverageRating);
+            Float f1 = userRatingsForTwoItems.get(user).getRating1() - userAverageRating;
+            Float f2 = userRatingsForTwoItems.get(user).getRating2() - userAverageRating;
+            Float p = f1 * f2;
+            sum += p;
+        }
+        return sum;
+    }
+
+    public static Float calculateDenominatorForSimilarityFunction(Integer item1,Integer item2){
+        HashMap<Integer, RatingTuple> userRatingsForTwoItems = getUserRatingsForTwoItems(item1, item2);
+        Float sum1 = 0.0f;
+        Float sum2 = 0.0f;
+        Float prod = 0.0f;
+
+        for (Integer user: userRatingsForTwoItems.keySet())
+        {
+            Float userAverageRating =  averageRatings.get(user);
+            Float f1 = userRatingsForTwoItems.get(user).getRating1() - userAverageRating;
+            Float f2 = userRatingsForTwoItems.get(user).getRating2() - userAverageRating;
+            sum1 += (float) Math.pow(f1, 2);
+            sum2 += (float) Math.pow(f2, 2);
+        }
+
+        prod = (float) Math.sqrt(sum1) * (float) Math.sqrt(sum2);
+        return prod;
+    }
+
+    public static Float calculateSimilarityBetweenTwoItems(Integer item1, Integer item2) {
+        Float numerator = calculateNumeratorForSimilarityFunction(item1, item2);
+        Float denominator = calculateDenominatorForSimilarityFunction(item1, item2);
+        return numerator / denominator;
+    }
+
+    public static HashMap<Integer, RatingTuple> getUserRatingsForTwoItems(int item1, int item2) {
+        HashMap<Integer, Float> userRatingsForItem1 = itemHM.get(item1);
+        HashMap<Integer, Float> userRatingsForItem2 = itemHM.get(item2);
+        //int count = 0;
+        HashMap<Integer, RatingTuple> userRatingsForTwoItems = new HashMap<Integer, RatingTuple>();
+        //System.out.println(userRatingsForTwoItems.size());
+
+        for (Integer user : userRatingsForItem1.keySet()) {
+            if (userRatingsForItem2.containsKey(user)) {
+                userRatingsForTwoItems.put(user, new RatingTuple(userRatingsForItem1.get(user), userRatingsForItem2.get(user)));
+                //System.out.println("User : " + user + " Rating for item 1: " + userRatingsForItem1.get(user) + " Rating for item 2: " + userRatingsForItem2.get(user));
+                //count++;
+
+            }
+        }
+        //System.out.println(userRatingsForTwoItems.size());
+        return userRatingsForTwoItems;
+    }
+
+    public static void calculateSimilarities() {
+        for (ItemTuple tuple : itemTuples) {
+            Float similarity = calculateSimilarityBetweenTwoItems(tuple.item1, tuple.item2);
+            //similarityTable.put(tuple, similarity);
+            System.out.println("Similarity between item " + tuple.item1 + " and item " + tuple.item2 + ": "+ similarity);
+        }
+    }
+
     public static void main(String[] args) {
         data = new HashMap<>();
 
         HashMap<Integer, Float> rating1 = new HashMap<>();
-        rating1.put(1, 3.5f);
-        rating1.put(2, 1.5f);
-        rating1.put(3, 2.0f);
+        rating1.put(1, 8f);
+        rating1.put(4, 2f);
+        rating1.put(5, 7f);
         HashMap<Integer, Float> rating2 = new HashMap<>();
         rating2.put(1, 2.0f);
-        rating2.put(2, 2.5f);
-        rating2.put(3, 4.0f);
+        rating2.put(3, 5f);
+        rating2.put(4, 7f);
+        rating2.put(5, 5f);
         HashMap<Integer, Float> rating3 = new HashMap<>();
         rating3.put(1, 5.0f);
-        rating3.put(2, 3.5f);
-        rating3.put(3, 3.0f);
+        rating3.put(2, 4f);
+        rating3.put(3, 7f);
+        rating3.put(4, 4f);
+        rating3.put(5, 7f);
         HashMap<Integer, Float> rating4 = new HashMap<>();
-        rating4.put(1, 2.5f);
-        rating4.put(2, 4.5f);
-        rating4.put(3, 4.0f);
+        rating4.put(1, 7f);
+        rating4.put(2, 1f);
+        rating4.put(3, 7.0f);
+        rating4.put(4, 3f);
+        rating4.put(5, 8f);
         HashMap<Integer, Float> rating5 = new HashMap<>();
-        rating5.put(1, 4.5f);
-        rating5.put(2, 2.5f);
-        rating5.put(3, 5.0f);
+        rating5.put(1, 1f);
+        rating5.put(2, 7f);
+        rating5.put(3, 4f);
+        rating5.put(4, 6f);
+        rating5.put(5, 5f);
+        HashMap<Integer, Float> rating6 = new HashMap<>();
+        rating6.put(1, 8f);
+        rating6.put(2, 3f);
+        rating6.put(3, 8f);
+        rating6.put(4, 3f);
+        rating6.put(5, 7f);
 
-        data.put(1000, rating1);
-        data.put(2000, rating2);
-        data.put(3000, rating3);
-        data.put(4000, rating4);
-        data.put(5000, rating5);
+        data.put(1, rating1);
+        data.put(2, rating2);
+        data.put(3, rating3);
+        data.put(4, rating4);
+        data.put(5, rating5);
+        data.put(6, rating6);
+
+
+        HashMap<Integer, Float> itemToUser1 = new HashMap<>();
+        itemToUser1.put(1, 8f);
+        itemToUser1.put(2, 2f);
+        itemToUser1.put(3, 5f);
+        itemToUser1.put(4, 7f);
+        itemToUser1.put(5, 1f);
+        itemToUser1.put(6, 8f);
+
+        HashMap<Integer, Float> itemToUser2 = new HashMap<>();
+        itemToUser2.put(3, 4f);
+        itemToUser2.put(4, 1f);
+        itemToUser2.put(5, 7f);
+        itemToUser2.put(6, 3f);
+
+        HashMap<Integer, Float> itemToUser3 = new HashMap<>();
+        itemToUser3.put(2, 5f);
+        itemToUser3.put(3, 7f);
+        itemToUser3.put(4, 7f);
+        itemToUser3.put(5, 4f);
+        itemToUser3.put(6, 8f);
+        HashMap<Integer, Float> itemToUser4 = new HashMap<>();
+        itemToUser4.put(1, 2f);
+        itemToUser4.put(2, 7f);
+        itemToUser4.put(3, 4f);
+        itemToUser4.put(4, 3f);
+        itemToUser4.put(5, 6f);
+        itemToUser4.put(6, 3f);
+        HashMap<Integer, Float> itemToUser5 = new HashMap<>();
+        itemToUser5.put(1, 7f);
+        itemToUser5.put(2, 5f);
+        itemToUser5.put(3, 7f);
+        itemToUser5.put(4, 8f);
+        itemToUser5.put(5, 5f);
+        itemToUser5.put(6, 7f);
 
         populateAveragesInMap();
 
@@ -99,11 +216,11 @@ public class Test {
             System.out.println("User: " + entry.getKey() + ", average rating: " + entry.getValue());
         }
 
-        itemHM.put(1, rating1);
-        itemHM.put(2, rating2);
-        itemHM.put(3, rating3);
-        itemHM.put(4, rating4);
-        itemHM.put(5, rating5);
+        itemHM.put(1, itemToUser1);
+        itemHM.put(2, itemToUser2);
+        itemHM.put(3, itemToUser3);
+        itemHM.put(4, itemToUser4);
+        itemHM.put(5, itemToUser5);
 
         getAllItemTuples();
 
@@ -111,7 +228,10 @@ public class Test {
             System.out.println("item 1: " + tuple.item1 + ", item 2: " + tuple.item2);
         }
 
-
+        calculateSimilarities();
+        //for (Map.Entry<ItemTuple, Float> entry : similarityTable.entrySet()) {
+            //System.out.print(entry.getValue());
+        //}
 
     }
 
